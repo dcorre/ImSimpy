c554edc01512f4ff41149edb1110ca8c5e8bd4e3
dcorre <corre@lal.in2p3.fr>
2020-08-05 12:03:36 +0200

    Updates to make use of Travis CI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
@@ -26,7 +26,9 @@ requirements = [
         'astroquery',
         'pyregion',
         'cython',
-        'pytest']
+        'pytest',
+        'pyGRBaglow',
+        'pyETC']
 
 setup_requirements = ['pytest-runner']
 
@@ -37,15 +39,17 @@ setup(
     author_email='david.corre.fr@gmail.com',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+
     ],
     description="Image Simulator for optical/NIR telescope",
     entry_points={
@@ -55,7 +59,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='ImSimpy',
     name='ImSimpy',
